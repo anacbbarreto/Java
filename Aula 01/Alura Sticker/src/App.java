26cccc99e1f40f113a0460fddf88408a98cc4ac1
anacbbarreto <anacbbarreto@yahoo.com.br>
2022-07-18 16:24:53 -0300

    Ajuste texto e classes

--- Aula 01/Alura Sticker/src/App.java
+++ Aula 01/Alura Sticker/src/App.java
@@ -9,25 +9,25 @@ import java.util.Map;
 public class App {
 
     public static void main(String[] args) throws Exception {
-
-        // fazer uma conexão HTTP e buscar os top 250 filmes
-        String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
-        URI endereco = URI.create(url);
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(endereco).GET().build();
-        final HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
-
-        // extrair só os dados que interessam (titulo, poster, classificação)
-        var parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
-
-        // exibir e manipular os dados 
-        for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
-            System.out.println();
-        }
+       
+         // fazer uma conexão HTTP e buscar os top 250 filmes
+         String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+         URI endereco = URI.create(url);
+         var client = HttpClient.newHttpClient();
+         var request = HttpRequest.newBuilder(endereco).GET().build();
+         final HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+         String body = response.body();
+ 
+         // extrair só os dados que interessam (titulo, poster, classificação)
+         var parser = new JsonParser();
+         List<Map<String, String>> listaDeFilmes = parser.parse(body);
+ 
+         // exibir e manipular os dados 
+         for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println("Filme: " + filme.get("title"));
+             System.out.println("Imagem: " + filme.get("image"));
+             System.out.println("Nota: " + filme.get("imDbRating"));
+             System.out.println();
+         }
+     }
     }
-}
